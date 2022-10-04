e0235160126bf3debf6cf1a4ef2be597489005ba
Yesha  Mavani <yesha.mavani@sourcefuse.com>
2022-10-04 12:46:34 +0530

    feat(chore): hhh
    GH-99

--- release-notes.js
+++ release-notes.js
@@ -1,24 +1,38 @@
 const releaseNotes = require('git-release-notes');
 const simpleGit = require('simple-git/promise');
+const path = require('path');
+const {readFile, writeFile, ensureFile} = require('fs-extra');
 
 async function generateReleaseNotes() {
   try {
     const OPTIONS = {
       branch: 'master',
     };
-    const RANGE = await build();
-    console.log(RANGE);
+    const RANGE = await getRange();
     const TEMPLATE = './mymarkdown.ejs';
 
     const changelog = await releaseNotes(OPTIONS, RANGE, TEMPLATE);
-    console.log(`Release Notes between ${RANGE}\n\n${changelog}`);
+    //console.log(`Release Notes between ${RANGE}\n\n${changelog}`);
+    const changelogPath = path.resolve(__dirname, 'CHANGELOG.md');
+    await ensureFile(changelogPath);
+    const currentFile = (await readFile(changelogPath)).toString().trim();
+    if (currentFile) {
+      console.log('Update %s', changelogPath);
+    } else {
+      console.log('Create %s', changelogPath);
+    }
+
+    await writeFile(changelogPath, changelog);
+    await writeFile(changelogPath, currentFile, {flag: 'a+'});
+    await addAndCommit();
+    console.log('here----');
   } catch (ex) {
     console.error(ex);
     process.exit(1);
   }
 }
 
-async function build() {
+async function getRange() {
   const git = simpleGit();
   const tags = (await git.tag()).split('\n');
   tags.pop();
@@ -26,6 +40,17 @@ async function build() {
   const startTag = tags.slice(-2)[0];
   const endTag = tags.slice(-1)[0];
   return `${startTag}..${endTag}`;
+}
+
+async function addAndCommit() {
+  const git = simpleGit();
+  await git.add(['CHANGELOG.md']);
+  await git.commit('chore(release): changelog file11', {'--no-verify': null});
+  await git.push('origin', 'master');
+  // await git.commit('chore(release): changelog file', ['CHANGELOG.md'], {
+  //   '--no-verify': null,
+  // });
+  //.push('origin', 'master');
 }
 
 generateReleaseNotes().catch(ex => {
