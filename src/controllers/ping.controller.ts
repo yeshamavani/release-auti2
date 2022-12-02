50199244b51c6863956810c62feacb8412137e5a
Yesha  Mavani <yesha.mavani@sourcefuse.com>
2022-12-02 12:39:14 +0530

    feat(component): change feature
    this is a feature change
     description change
     line three
    
    GH-45

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -46,7 +46,7 @@ export class PingController {
   ping(): object {
     // Reply with a greeting, the current time, the url, and request headers
     return {
-      greeting: 'Hello from LoopBack33',
+      greeting: 'Hello from LoopBack66',
       date: new Date(),
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
