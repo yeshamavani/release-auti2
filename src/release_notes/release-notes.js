3a27eeebbe58ae126223771bcd2fad3e598f1548
Yesha Mavani <yesha.mavani@sourcefuse.com>
2024-05-14 00:02:29 +0530

    feat(provider): kbgjh
    edgv
    
    GH-65

--- src/release_notes/release-notes.js
+++ src/release_notes/release-notes.js
@@ -1,63 +1,59 @@
-// const releaseNotes = require('git-release-notes');
-// const simpleGit = require('simple-git/promise');
-// const path = require('path');
-// const {readFile, writeFile, ensureFile} = require('fs-extra');
+const releaseNotes = require('git-release-notes');
+const simpleGit = require('simple-git/promise');
+const path = require('path');
+const {readFile, writeFile, ensureFile} = require('fs-extra');
 
-// module.exports = async () => {
-//   try {
-//     const OPTIONS = {
-//       branch: 'master',
-//       s: './post-processing.js',
-//     };
-//     const RANGE = await getRange();
-//     const TEMPLATE = './mymarkdown.ejs';
+module.exports = async () => {
+  try {
+    const OPTIONS = {
+      branch: 'master',
+      s: './src/release_notes/post-processing.js',
+    };
+    const RANGE = await getRange();
+    const TEMPLATE = './src/release_notes/mymarkdown.ejs';
 
-//     const changelog = await releaseNotes(OPTIONS, RANGE, TEMPLATE);
-//     //console.log(`Release Notes between ${RANGE}\n\n${changelog}`);
-//     const changelogPath = path.resolve(__dirname, 'CHANGELOG.md');
-//     await ensureFile(changelogPath);
-//     const currentFile = (await readFile(changelogPath)).toString().trim();
-//     if (currentFile) {
-//       console.log('Update %s', changelogPath);
-//     } else {
-//       console.log('Create %s', changelogPath);
-//     }
+    const changelog = await releaseNotes(OPTIONS, RANGE, TEMPLATE);
+    //console.log(`Release Notes between ${RANGE}\n\n${changelog}`);
+    const changelogPath = path.resolve(__dirname, '../..', 'CHANGELOG.md');
+    await ensureFile(changelogPath);
+    const currentFile = (await readFile(changelogPath)).toString().trim();
+    if (currentFile) {
+      console.log('Update %s', changelogPath);
+    } else {
+      console.log('Create %s', changelogPath);
+    }
 
-//     await writeFile(changelogPath, changelog);
-//     await writeFile(changelogPath, currentFile, {flag: 'a+'});
-//     await addAndCommit().then(() => {
-//       console.log('Changelog has been updated');
-//     });
-//   } catch (ex) {
-//     console.error(ex);
-//     process.exit(1);
-//   }
-// };
+    await writeFile(changelogPath, changelog);
+    await writeFile(changelogPath, currentFile, {flag: 'a+'});
+    await addAndCommit().then(() => {
+      console.log('Changelog has been updated');
+    });
+  } catch (ex) {
+    console.error(ex);
+    process.exit(1);
+  }
+};
 
-// async function getRange() {
-//   const git = simpleGit();
-//   const tags = (await git.tag({'--sort': 'committerdate'})).split('\n');
-//   tags.pop();
+async function getRange() {
+  const git = simpleGit();
+  const tags = (await git.tag({'--sort': 'committerdate'})).split('\n');
+  tags.pop();
 
-//   const startTag = tags.slice(-2)[0];
-//   const endTag = tags.slice(-1)[0];
-//   return `${startTag}..${endTag}`;
-// }
+  const startTag = tags.slice(-2)[0];
+  const endTag = tags.slice(-1)[0];
+  return `${startTag}..${endTag}`;
+}
 
-// async function addAndCommit() {
-//   const git = simpleGit();
-//   await git.add(['CHANGELOG.md']);
-//   await git.commit('chore(release): changelog file', {
-//     '--no-verify': null,
-//   });
-//   await git.push('origin', 'master');
-// }
+async function addAndCommit() {
+  const git = simpleGit();
+  await git.add(['CHANGELOG.md']);
+  await git.commit('chore(release): changelog file', {
+    '--no-verify': null,
+  });
+  await git.push('origin', 'master');
+}
 
 // generateReleaseNotes().catch(ex => {
 //   console.error(ex);
 //   process.exit(1);
 // });
-
-module.exports = async () => {
-  console.log('helloooo');
-};
